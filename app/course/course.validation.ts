c028f0f9c8b42e1c681e0f00fa15e4b0ab5ed7b5
Abhay Madaan <madaanabhay9@gmail.com>
2025-01-21 22:39:27 +0530

    added rest course validation

--- app/course/course.validation.ts
+++ app/course/course.validation.ts
@@ -23,6 +23,46 @@ export const createCourse = [
     .withMessage("Duration must be a positive integer"),
 ];
 
-export const updateCourse = [];
+export const updateCourse = [
+  body("name")
+    .notEmpty()
+    .withMessage("Course name is required")
+    .isString()
+    .withMessage("Course name must be a string")
+    .custom(async (value) => {
+      const isCourseExists = await courseService.getCourseByName(value);
+      if (isCourseExists.length) {
+        throw new Error("Course already exists");
+      }
+    }),
+  body("description")
+    .optional()
+    .isString()
+    .withMessage("Description must be a string"),
+  body("duration")
+    .notEmpty()
+    .withMessage("Duration is required")
+    .isInt({ gt: 0 })
+    .withMessage("Duration must be a positive integer"),
+];
 
-export const editCourse = [];
+export const editCourse = [
+  body("name")
+  .optional()
+  .isString()
+  .withMessage("Course name must be a string")
+  .custom(async (value) => {
+    const isCourseExists = await courseService.getCourseByName(value);
+    if (isCourseExists.length) {
+      throw new Error("Course already exists");
+    }
+  }),
+body("description")
+  .optional()
+  .isString()
+  .withMessage("Description must be a string"),
+body("duration")
+  .optional()
+  .isInt({ gt: 0 })
+  .withMessage("Duration must be a positive integer"),
+];
